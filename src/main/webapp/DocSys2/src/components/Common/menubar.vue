069cbd0237b37a4e5ed5056e6f6b1042a9d344bd
Administrator <nelsonnick@126.com>
2019-02-20 15:11:45 +0800

    。

--- src/main/webapp/DocSys2/src/components/Common/menubar.vue
+++ src/main/webapp/DocSys2/src/components/Common/menubar.vue
@@ -3,39 +3,23 @@
     <div class="layout-nav">
       <MenuItem name="main">
         <Icon type="user"></Icon>
-        当前用户：{{ userName }}
+        欢迎：{{ userName }}
       </MenuItem>
-      <MenuItem name="personList">
+      <MenuItem name="IntegratedManagement">
         <Icon type="android-person"></Icon>
-        困难人员
+        综合管理
       </MenuItem>
-      <MenuItem name="familyList">
+      <MenuItem name="SystemManagement">
         <Icon type="android-people"></Icon>
-        家庭成员
+        系统管理
       </MenuItem>
-      <Submenu name="change">
-        <template slot="title">
-          <Icon type="cube"></Icon>
-          变更记录
-        </template>
-        <MenuItem name="personChange">困难人员</MenuItem>
-        <MenuItem name="familyChange">家庭成员</MenuItem>
-      </Submenu>
-      <MenuItem name="user" v-if="sys">
+      <MenuItem name="UserManagement">
         <Icon type="ios-people-outline"></Icon>
         用户管理
       </MenuItem>
-      <MenuItem name="message">
-        <Icon type="chatbubbles"></Icon>
-        消息管理
-      </MenuItem>
-      <MenuItem name="pass">
-        <Icon type="android-settings"></Icon>
-        修改密码
-      </MenuItem>
       <MenuItem name="logout">
         <Icon type="power"></Icon>
-        退出系统
+        退出登录
       </MenuItem>
     </div>
   </Menu>
@@ -44,31 +28,11 @@
   import * as BASE from './Base.js'
   export default {
     name: 'menubar',
-    props: ['sys', 'active', 'userName'],
+    props: [ 'active', 'userName'],
     methods: {
       MenuClick (name) {
         if (name.toString() === 'personList') {
           window.location.href = BASE.base + 'person'
-        } else if (name.toString() === 'personChange') {
-          window.location.href = BASE.base + 'personChange'
-        } else if (name.toString() === 'familyList') {
-          window.location.href = BASE.base + 'family'
-        } else if (name.toString() === 'familyChange') {
-          window.location.href = BASE.base + 'familyChange'
-        } else if (name.toString() === 'pass') {
-          window.location.href = BASE.base + 'pass'
-        } else if (name.toString() === 'user') {
-          window.location.href = BASE.base + 'user'
-        } else if (name.toString() === 'message') {
-          window.location.href = BASE.base + 'message'
-        } else if (name.toString() === 'logout') {
-          window.location.href = BASE.base + 'logout'
-        } else if (name.toString() === 'main') {
-
-        } else if (name.toString() === 'person') {
-
-        } else if (name.toString() === 'family') {
-
         } else {
           window.location.href = BASE.base
         }
